50bcee12f80a7f35e2fd9b2a33428e61c3aada97
Bobby Rachkov <bobisvetoslavovrachkov@hotmail.com>
2018-05-17 23:55:21 +0300

    Add project files.

---
+++ EnhancePointerPrecisionToggler/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
+
+namespace EnhancePointerPrecisionToggler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly NotifyIcon _icon;
+        private readonly RegistryKey _rk;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Hide();
+            _icon = new NotifyIcon();
+            _rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            _rk.SetValue("EnhancePointerPrecisionToggler", Assembly.GetEntryAssembly().Location);
+
+            Redraw();
+            _icon.Visible = true;
+            _icon.ContextMenu = new ContextMenu();
+
+            _icon.MouseClick += Click;
+            _icon.ContextMenu.Popup += Exit;
+        }
+
+        private void Redraw()
+        {
+            if (Win32Wrapper.IsToggled)
+            {
+                _icon.Icon = Properties.Resources.mouse_green;
+            }
+            else
+            {
+                _icon.Icon = Properties.Resources.mouse_red;
+            }
+        }
+
+        private void Click(object sender, EventArgs eventArgs)
+        {
+            Win32Wrapper.IsToggled = !Win32Wrapper.IsToggled;
+            Redraw();
+        }
+
+
+        private void Exit(object sender, EventArgs mouseEventArgs)
+        {
+            Win32Wrapper.IsToggled = !Win32Wrapper.IsToggled;
+            _rk.DeleteValue("EnhancePointerPrecisionToggler", false);
+            Close();
+        }
+
+        ~MainWindow()
+        {
+            _icon.Dispose();
+        }
+    }
+}
---
+++ EnhancePointerPrecisionToggler/Win32Wrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace EnhancePointerPrecisionToggler
+{
+    public static class Win32Wrapper
+    {
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoGet(uint action, uint param, IntPtr vparam, Spif fWinIni);
+
+        public const UInt32 SpiGetmouse = 0x0003;
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoSet(uint action, uint param, IntPtr vparam, Spif fWinIni);
+
+        public const UInt32 SpiSetmouse = 0x0004;
+
+        public static bool IsToggled
+        {
+            get
+            {
+                int[] mouseParams = new int[3];
+                SystemParametersInfoGet(SpiGetmouse, 0,
+                    GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), 0);
+                return mouseParams[2] == 1;
+            }
+            set
+            {
+                int[] mouseParams = new int[3];
+                SystemParametersInfoGet(SpiGetmouse, 0,
+                    GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), 0);
+                mouseParams[2] = value ? 1 : 0;
+                SystemParametersInfoSet(SpiSetmouse, 0,
+                    GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), Spif.SendChange);
+            }
+        }
+
+        private static bool ToggleEnhancePointerPrecision(bool b)
+        {
+            //https://stackoverflow.com/questions/24737775/toggle-enhance-pointer-precision
+            int[] mouseParams = new int[3];
+            // Get the current values.
+            SystemParametersInfoGet(SpiGetmouse, 0,
+                GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), 0);
+            // Modify the acceleration value as directed.
+            mouseParams[2] = b ? 1 : 0;
+            // Update the system setting.
+            return SystemParametersInfoSet(SpiSetmouse, 0,
+                GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), Spif.SendChange);
+        }
+    }
+
+    [Flags]
+    public enum Spif
+    {
+        None = 0x00,
+
+        /// <summary>Writes the new system-wide parameter setting to the user profile.</summary>
+        UpdateIniFile = 0x01,
+
+        /// <summary>Broadcasts the WM_SETTINGCHANGE message after updating the user profile.</summary>
+        SendChange = 0x02,
+
+        /// <summary>Same as SendChange.</summary>
+        SendWinIniChange = 0x02
+    }
+}
